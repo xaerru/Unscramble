70f8bc7c6d53d6891788fd5f748734a8f312c3c1
GauravsinghSisodia <65955464+Gauravsinghsisodia@users.noreply.github.com>
2020-08-22 22:19:34 +0530

    Update scramble.py

--- scramble.py
+++ scramble.py
@@ -2,7 +2,7 @@ l=open("scrabble.txt").readlines()
 p=[x.lower().rstrip() for x in l]
 w=input("Enter the word you want to unscramble:\n").lower()
 a=sorted([x for x in p if (x in w)or(sorted(x)==sorted(w))],key=len)
-q=list(set([len(x) for x in a]))[1:]
+q=list(set([len(x) for x in a]))[2:]
 for y in q:
     print(f"\nThe word/s of length {y} formed by '{w}' are:")
     for x in a:
