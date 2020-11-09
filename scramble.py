ad0d9466fc4e2f6230143c8a0a10dc947b0dac72
Gauravsingh <gauravsinghsisodia5@gmail.com>
2020-11-09 11:19:31 +0530

    Made scramble.py shorter

--- scramble.py
+++ scramble.py
@@ -1,10 +1,5 @@
-l=open("scrabble.txt").readlines()
-p=[x.lower().rstrip() for x in l]
-w=input("Enter the word you want to unscramble:\n").lower()
-a=sorted([x for x in p if (x in w)or(sorted(x)==sorted(w))],key=len)
-q=list(set([len(x) for x in a]))[2:]
-for y in q:
+l=open("scrabble.txt").readlines();p=[x.lower().rstrip() for x in l];w=input("Enter the word you want to unscramble:\n").lower()
+for y in list(set([len(x) for x in sorted([x for x in p if (x in w)or(sorted(x)==sorted(w))],key=len)]))[2:]:
     print(f"\nThe word/s of length {y} formed by '{w}' are:")
-    for x in a:
-        if len(x)==y:
-            print(x,end="\n")
+    for x in sorted([x for x in p if (x in w)or(sorted(x)==sorted(w))],key=len):
+        if len(x)==y:print(x,end="\n")
